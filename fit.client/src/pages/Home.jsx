ee68f26c1627aea5d54d16dc2405240be3cf7d9f
BastiMagOrangen <bastian.seidl@hotmail.com>
2023-01-31 21:56:01 +0100

    add Home Site

---
+++ fit.client/src/pages/Home.jsx
@@ -0,0 +1,8 @@
+export default function Home() {
+
+    return (
+        <div>
+            <h1 className="text-center mt-10 font-bold text-primary underline text-3xl">Willkommen zum Fit-Anmeldesystem</h1>
+        </div>
+    );
+}
