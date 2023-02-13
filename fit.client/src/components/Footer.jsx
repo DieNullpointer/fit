8d8ac2757aed3e0ea3be2a922bb3cf873862ec62
phil_not_funny <schrenk.philip@hotmail.com>
2023-02-13 10:20:37 +0100

    feat: added Footer

--- fit.client/src/components/Footer.jsx
+++ fit.client/src/components/Footer.jsx
@@ -1,8 +1,54 @@
 import Logo from "./atoms/Logo";
+//import Label from "./Label";
 import { Link } from "react-router-dom";
 
-export default function Footer() {
-  return (
-    <></>
+export default function Footer({ oldschool }) {
+  return oldschool ? (
+    <div className="bg-dark p-3 items-center w-full">
+      <div className="w-full flex justify-around text-white">
+        <div>&copy; HTL Spengergasse</div>
+        <div>
+          <Link className="hover:underline mr-2" to="/datenschutz">
+            Datenschutz
+          </Link>
+
+          <Link className="hover:underline" to="/impressum">
+            Impressum
+          </Link>
+        </div>
+      </div>
+    </div>
+  ) : (
+    <div className="bg-primary p-2 items-center w-full py-6">
+      <div className="w-full grid grid-cols-1 sm:grid-cols-3">
+        <div className="flex justify-center items-center">
+          <span className="text-xl text-dimmed font-okineLight pr-1">
+            Im Auftrag der
+          </span>
+          <a
+            rel="noreferrer"
+            className="text-xl text-dimmed hover:underline font-okine"
+            target="_blank"
+            href="https://www.spengergasse.at"
+          >
+            Spengergasse
+          </a>
+        </div>
+        <div className="flex justify-center items-center">
+          <Logo size="md" white />
+        </div>
+        <div className="flex justify-center items-center">
+          <Link
+            className="text-xl text-dimmed hover:underline font-okine"
+            to="/Impressum"
+          >
+            Impressum
+          </Link>
+        </div>
+      </div>
+      <p className="text-dimmed font-okineLight text-center text-sm mt-2">
+        &copy; Nullpointer, 2023
+      </p>
+    </div>
   );
 }
--- fit.client/src/pages/Home.jsx
+++ fit.client/src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import Button from "../components/atoms/Button";
 import Input from "../components/atoms/Input";
 import Select from "../components/atoms/Select";
+import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
 
 export default function Home() {
   return (
-    <>
+    <div>
+      <div className="min-h-screen">
       <Navbar pages={[{name: "sign-up"}, {name: "about"}]} profileSettings />
       <div className="flex flex-col space-y-4 m-5">
         <Button text="Button" sharp />
@@ -26,6 +28,8 @@ export default function Home() {
         />
         <Select options={["Herbert", "Louis", "Franz", "yusuftoprak"]} label="Name" id="name"  />
       </div>
-    </>
+      </div>
+      <Footer oldschool />
+    </div>
   );
 }
