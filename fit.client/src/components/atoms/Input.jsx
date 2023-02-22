d9d95a7116c4f4bf9dac83d9e5de9f9b0f59840a
phil_not_funny <schrenk.philip@hotmail.com>
2023-02-22 10:27:48 +0100

    feat: added comp. name and comp. address to signup

--- fit.client/src/components/atoms/Input.jsx
+++ fit.client/src/components/atoms/Input.jsx
@@ -2,8 +2,9 @@ import styled from "@emotion/styled";
 import { TextField } from "@mui/material";
 import Style from "../../styleConstants";
 
-export default function Input({required, value, label, type, purpose, id, onChange, size }) {
+export default function Input({required, value, label, type, purpose, id, onChange, size, block, full }) {
   const ColorTextField = styled(TextField)(({ theme }) => ({
+    display: block ? 'block' : 'inline-block',
     '& label.Mui-focused': {
       color: Style.colors.dark,
     },
@@ -29,11 +30,11 @@ export default function Input({required, value, label, type, purpose, id, onChan
       onChange={onChange}
       label={label}
       required={required}
-      type={type ||"text"}
+      type={type || "text"}
       defaultValue={value}
       autoComplete={purpose === "password" ? "current-password" : purpose}
       variant="standard"
-      size={size || "full"}
+      fullWidth={full}
     />
   );
 }
--- fit.client/src/pages/Signup.jsx
+++ fit.client/src/pages/Signup.jsx
@@ -3,9 +3,10 @@ import { Box } from "@mui/system";
 import Paper from "../components/atoms/Paper";
 import PageFrame from "../components/PageFrame";
 import { useForm } from "react-hook-form";
+import Input from "../components/atoms/Input";
 
 export default function Signup({}) {
-  const { signup, handleSubmit, formState: errors } = useForm();
+  const { register, handleSubmit, formState: errors } = useForm();
   const handleSignup = (data) => console.log(data);
 
   return (
@@ -17,11 +18,38 @@ export default function Signup({}) {
         </Typography>
       </Box>
       <Paper elevation={3} className="mt-12">
-        <div className="flex justify-center items-center">
-          <form onSubmit={handleSubmit(handleSignup)}>
-            abc
-          </form>
-        </div>
+        <form onSubmit={handleSubmit(handleSignup)} className="py-4 px-8">
+          <div className="grid grid-cols-1 md:grid-cols-2 w-full">
+            <div className="w-full">
+              <Typography variant="h6">Firmendetails</Typography>
+              <div className="pl-3 w-full">
+                <div className="mt-2 border-l border-l-dark px-4 py-2 w-full">
+                  <Input
+                    id="in-comp-name"
+                    label="Firmenname"
+                    {...register("name")}
+                    purpose={"text"}
+                    required
+                    block
+                    full
+                  />
+                  <Input
+                    id="in-comp-address"
+                    label="Firmenadresse"
+                    {...register("address")}
+                    purpose={"text"}
+                    required
+                    block
+                    full
+                  />
+                </div>
+              </div>
+            </div>
+            <div>
+
+            </div>
+          </div>
+        </form>
       </Paper>
     </PageFrame>
   );
