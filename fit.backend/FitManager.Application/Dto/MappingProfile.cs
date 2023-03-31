2b1b444016f86fe629dfca63d12cb4054c2fbaf3
Bastian Seidl <sei20375@spengergasse.at>
2023-03-31 10:42:10 +0200

    Bug Fix

--- fit.backend/FitManager.Application/Dto/MappingProfile.cs
+++ fit.backend/FitManager.Application/Dto/MappingProfile.cs
@@ -11,7 +11,6 @@ namespace FitManager.Application.Dto
             CreateMap<CompanyCmd, Company>(); //CompanyCmd --> Company
             CreateMap<EventCmd, Event>(); //EventCmd --> Event
             CreateMap<PackageCmd, Package>(); //PackageCmd --> Package
-            CreateMap<Event, AllEventDto>().ForMember(dest => dest.Date, act => act.MapFrom(src => (src.Date.ToString("dd/MM/yyyy")))); //Event --> AllEventDto
         }
     }
 }
