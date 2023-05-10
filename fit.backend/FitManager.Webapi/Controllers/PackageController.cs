550ca19388319e251655006148ae2adedac6b68a
Philip Schrenk <SCH22538@spengergasse.at>
2023-05-10 15:46:11 +0200

    fix: local fix for redirect problem

--- fit.backend/FitManager.Webapi/Controllers/PackageController.cs
+++ fit.backend/FitManager.Webapi/Controllers/PackageController.cs
@@ -2,6 +2,7 @@
 using FitManager.Application.Dto;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
 
 using System.Linq;
 using FitManager.Application.Services;
