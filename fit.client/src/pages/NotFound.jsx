1e0dc73cce7b7cc8e6e8d36d13797085f863f640
Louis Muhr <louismuhr8@gmail.com>
2023-05-12 10:40:13 +0200

    feat: 404 page

--- fit.client/src/pages/NotFound.jsx
+++ fit.client/src/pages/NotFound.jsx
@@ -1,7 +1,14 @@
+import PageFrame from "../components/PageFrame";
+import { Typography } from "@mui/material";
+import { Link } from "react-router-dom";
+
 
 export default function NotFound()
 {
     return(
-        <h1>404 Not Found</h1>
+    <PageFrame active="home" margin>
+      <Typography variant="h3" center>404 Not Found</Typography>
+      <Typography variant="subtitle2">Back to Homepage<Link to={"/"}> /home</Link></Typography>
+    </PageFrame>
     );
 }
