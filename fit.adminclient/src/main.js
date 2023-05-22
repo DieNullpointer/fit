d0c3ed19716bfd2809405d6ec312ecfd44b2325a
Maximilian Schwarz <sch22384@spengergasse.at>
2023-05-22 11:12:55 +0200

    feat: added change Button for Packages

--- fit.adminclient/src/main.js
+++ fit.adminclient/src/main.js
@@ -6,6 +6,7 @@ import router from './router'
 import store from './store.js'
 import process from 'node:process'
 import './assets/main.css'
+import 'primeicons/primeicons.css';
 import "primevue/resources/themes/lara-light-indigo/theme.css";     
 import "primevue/resources/primevue.min.css";
 
