2980ecc7c820c37c641378618f4bac79d6094e33
Bastian Seidl <sei20375@spengergasse.at>
2023-06-06 11:02:38 +0200

    date bug fix

--- fit.backend/FitManager.Application/Dto/EventDto.cs
+++ fit.backend/FitManager.Application/Dto/EventDto.cs
@@ -12,6 +12,6 @@ namespace FitManager.Application.Dto
         Guid Guid,
         [StringLength(255, MinimumLength = 1, ErrorMessage = "Die Länge des Namens ist ungültig.")]
         string Name,
-        string Date
+        DateTime Date
     );
 }
